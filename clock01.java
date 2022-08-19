86758e853e86484b623d90f29fe0459692c7e52b
Georgiy Garenov <107962152+GGarenov@users.noreply.github.com>
2022-08-19 07:09:46 +0300

    Add files via upload

---
+++ clock01.java
@@ -0,0 +1,16 @@
+package nastedLoop;
+
+import java.util.Scanner;
+
+public class clock01 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        for (int h = 0; h <=23; h++) {
+            for (int m = 0; m <=59; m++) {
+                System.out.printf("%d:%d%n", h, m);
+                
+            }
+
+        }
+    }
+}
---
+++ combinations03.java
@@ -0,0 +1,27 @@
+package nastedLoop;
+
+import java.util.Scanner;
+
+public class combinations03 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = Integer.parseInt(scanner.nextLine());
+        int totalCount = 0;
+        int validCombCount = 0;
+        for (int x1 = 0; x1 <=n; x1++) {
+            for (int x2 = 0; x2 <=n; x2++) {
+                for (int x3 = 0; x3 <=n ; x3++) {
+                    totalCount++;
+                    int sum = x1 + x2 + x3;
+                    if (sum ==n){
+                        validCombCount++;
+                    }
+                }
+
+            }
+
+        }
+
+        System.out.println(validCombCount);
+    }
+}
